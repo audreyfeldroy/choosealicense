8cc3a9d533f702d12e2a9f1e009a64830d203485
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2016-06-04 01:19:05 -0700

    First pass at ChooseALicensePlugin class definition.

--- choosealicense/__init__.py
+++ choosealicense/__init__.py
@@ -3,3 +3,34 @@
 __author__ = 'Audrey Roy Greenfeld'
 __email__ = 'aroy@alum.mit.edu'
 __version__ = '0.1.0'
+
+
+from cookiecutter.generate import generate_file
+from cookiecutter.plugins import CookiecutterPlugin
+
+
+class ChooseALicensePlugin(CookiecutterPlugin):
+    """Plugin for choosing a license, then outputting the LICENSE file into
+    the generated project's root directory"""
+
+    extra_prompts = {
+    	"open_source_license": [
+            "MIT",
+            "BSD",
+            "ISCL",
+            "Apache Software License 2.0",
+            "Not open source"
+        ]
+    }
+
+    def insert_license():
+        """
+        Given a license choice, insert the corresponding license file into the
+        generated project's root.
+        """
+        generate_file(
+            project_dir="..",
+            infile="LICENSE",
+            context=self.context,
+            env=self.env
+        )
